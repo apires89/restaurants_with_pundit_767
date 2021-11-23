0cd8d806198e834990b997a9202c364181f6d049
andre <andrecabralpires@gmail.com>
2021-11-23 09:27:54 +0000

    lecture

---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,27 @@
+class RestaurantPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+
+
+  def update?
+    ## record --> instance of restaurant
+    ## user --> current_user
+    record.user == user || user.admin
+  end
+
+  def destroy?
+    record.user == user || user.admin
+  end
+
+end
